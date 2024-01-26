79a0e7b68a12dc6e4fc0cf3b21fd4f594431bfb2
Emily <emily.zy.lin@gmail.com>
2024-01-26 16:11:36 -0600

    imported auth from firebase to SignIn.jsx

--- frontend/foodsharing/src/components/auth/SignIn.jsx
+++ frontend/foodsharing/src/components/auth/SignIn.jsx
@@ -1,14 +1,30 @@
+import {signInWithEmailAndPassword} from "firebase/auth";
 import React,{useState} from 'react';
+import {auth} from "../../firebase";
 
 const SignIn = () => {
     const [email,setEmail] = useState('');
     const [password,setPassword] = useState('');
+
+    const signIn = (e) => {
+       e.preventDefult();
+       //prevent the form submission from happening until all inputs have valid values
+       signInWithEmailAndPassword(auth,email,password)
+       .then((userCredential) => {
+        console.log(userCredential);
+       })
+        .catch((error) => {
+            console.log(error);
+        })
+    }
+
     return (
         <div className="sign-in-form">
-            <form>
+            <form onSubmit ={signIn}>
                 <h1>Sign In</h1>
                 <input type="email" placeholder="Enter your email" value={email} onChange={(e) => setEmail(e.target.value)}></input>
                 <input type="password" placeholder="Enter your password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
+                <button type="submit">Log In</button>
             </form>
         </div>
     );
