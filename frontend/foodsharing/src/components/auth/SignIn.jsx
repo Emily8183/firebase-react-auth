0b956da05f3fed50b9c72fc91752ed0264df1d95
Emily <emily.zy.lin@gmail.com>
2024-01-26 14:54:46 -0600

    added handler for the useState in the SignIn form

--- frontend/foodsharing/src/components/auth/SignIn.jsx
+++ frontend/foodsharing/src/components/auth/SignIn.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 const SignIn = () => {
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
     return (
-        <div></div>
-    )
-}
+        <div className="sign-in-form">
+            <form>
+                <h1>Sign In</h1>
+                <input type="email" placeholder="Enter your email" value={email} onChange={(e) => setEmail(e.target.value)}></input>
+                <input type="password" placeholder="Enter your password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
+            </form>
+        </div>
+    );
+};
+
+export default SignIn;
---
+++ frontend/foodsharing/src/filebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBWg2UcWyrTZQis1bVqQDJwLEGOlxG5ObE",
+  authDomain: "foodsharing-27d66.firebaseapp.com",
+  projectId: "foodsharing-27d66",
+  storageBucket: "foodsharing-27d66.appspot.com",
+  messagingSenderId: "208392563674",
+  appId: "1:208392563674:web:f01dd96075f2a481092ee3",
+  measurementId: "G-6F1FN2CXC4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
