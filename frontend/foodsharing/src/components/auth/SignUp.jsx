b0ed607c1fd5d8e2f6a1ceb529720368a61efe6a
Emily <emily.zy.lin@gmail.com>
2024-01-26 16:33:12 -0600

    created SignUp.jsx with firebase auth

---
+++ frontend/foodsharing/src/components/auth/SignUp.jsx
@@ -0,0 +1,33 @@
+import {createUserWithEmailAndPassword} from "firebase/auth";
+import React,{useState} from 'react';
+import {auth} from "../../firebase";
+
+const SignUp = () => {
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+
+    const signUp = (e) => {
+       e.preventDefult();
+       //prevent the form submission from happening until all inputs have valid values
+       createUserWithEmailAndPassword(auth,email,password)
+       .then((userCredential) => {
+        console.log(userCredential);
+       })
+        .catch((error) => {
+            console.log(error);
+        })
+    }
+
+    return (
+        <div className="sign-up-form">
+            <form onSubmit ={signUp}>
+                <h1>Sign Up</h1>
+                <input type="email" placeholder="Enter your email" value={email} onChange={(e) => setEmail(e.target.value)}></input>
+                <input type="password" placeholder="Enter your password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
+                <button type="submit">Sign Up</button>
+            </form>
+        </div>
+    );
+};
+
+export default SignUp;
