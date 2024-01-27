83805c411a337761152cbf6a452cafc1147bcaa0
Emily <emily.zy.lin@gmail.com>
2024-01-26 23:53:41 -0600

    added a statement on homepage if the user is logged in

--- frontend/foodsharing/src/App.js
+++ frontend/foodsharing/src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import SignIn from './components/auth/SignIn';
 import SignUp from "./components/auth/SignUp";
+import AuthDetails from './components/AuthDetails';
 
 function App() {
   return (
     <div className='App'>
       <SignUp />
       <SignIn />
-     
+      <AuthDetails />
     </div>
   );
 }
