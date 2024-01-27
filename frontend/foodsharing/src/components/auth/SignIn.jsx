548661acd595cf713d4703101d2afe0aae18030f
Emily <emily.zy.lin@gmail.com>
2024-01-27 00:26:10 -0600

    done the Log In statement's updating feature

--- frontend/foodsharing/src/components/auth/SignIn.jsx
+++ frontend/foodsharing/src/components/auth/SignIn.jsx
@@ -3,8 +3,8 @@ import React,{useState} from 'react';
 import {auth} from "../../firebase";
 
 const SignIn = () => {
-    const [email,setEmail] = useState('');
-    const [password,setPassword] = useState('');
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
 
     const signIn = (e) => {
        e.preventDefault();
